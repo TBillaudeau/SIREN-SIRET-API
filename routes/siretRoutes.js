0df06cc17b206eccad1b222cf2c524d232a1612a
Thomas <47186265+TBillaudeau@users.noreply.github.com>
2023-11-29 11:39:24 +0100

    Add Swagger doc

--- routes/siretRoutes.js
+++ routes/siretRoutes.js
@@ -2,7 +2,46 @@ const express = require('express');
 const router = express.Router();
 const siretController = require('../controllers/siretController');
 
+/**
+ * @swagger
+ * /siret/{siret}:
+ *   get:
+ *     summary: Retrieve a single SIRET
+ *     tags: [Siret]
+ *     parameters:
+ *       - in: path
+ *         name: siret
+ *         required: true
+ *         description: Numeric ID of the SIRET to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: A single SIRET.
+ *       404:
+ *         description: SIRET not found.
+ */
 router.get('/:siret', siretController.getSiret);
+
+/**
+ * @swagger
+ * /siret/{siret}:
+ *   delete:
+ *     summary: Delete a SIRET
+ *     tags: [Siret]
+ *     parameters:
+ *       - in: path
+ *         name: siret
+ *         required: true
+ *         description: Numeric ID of the SIRET to delete.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: SIRET deleted successfully.
+ *       404:
+ *         description: SIRET not found.
+ */
 router.delete('/:siret', siretController.deleteSiret);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 const siretRoutes = require('./routes/siretRoutes');
+
 const app = express();
 const port = 3000;
 
-//? Use the routes (defined in routes/siretRoutes.js)
+const options = {
+    definition: {
+      info: {
+        title: 'SIRET API',
+        version: '1.0.0',
+        description: 'API - Base of companies and their establishments (SIREN, SIRET)',
+      },
+    },
+    apis: ['./routes/*.js'],
+  };
+
 app.use(express.json());
 app.use('/siret', siretRoutes);
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(options)));
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
