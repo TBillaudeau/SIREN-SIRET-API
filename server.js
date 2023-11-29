01ead6d391322103ecce358e22a5f2e87bc8cb15
LuiAr <louis.arbey@efrei.net>
2023-11-29 11:36:54 +0100

    Added comments for server file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const siretRoutes = require('./routes/siretRoutes');
 const app = express();
 const port = 3000;
 
+//? Use the routes (defined in routes/siretRoutes.js)
 app.use(express.json());
 app.use('/siret', siretRoutes);
 
